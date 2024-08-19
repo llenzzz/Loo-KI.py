331641c88cb1404795a23019597527be8d3bc6dc
llenzzzz <llenzzz.cs@gmail.com>
2024-08-19 22:38:16 +0800

    Merge api_manager into file_hash
    More efficient checking for available api keys

--- file_hash.py
+++ file_hash.py
@@ -1,17 +1,29 @@
+import os
 import requests
-import api_manager
+from dotenv import load_dotenv
+
+def get_virustotalkey(index):
+    key = os.getenv(f'VIRUSTOTAL_API_{index+1}')
+    if key is None:
+        return None, None
+    else:
+        return index+1, key
 
 def virustotal(file_hash):
-    api_key = api_manager.virustotal()
+    load_dotenv('virustotal.env')
     file_hash = file_hash.strip().lower()
+    
+    index = 1
+    while True:
 
-    if not api_key:
-        raise ValueError("Error: No VirusTotal API Key")
-
-    headers = {"x-apikey": api_key}
-    response = requests.get(f"https://www.virustotal.com/api/v3/files/{file_hash}", headers=headers)
-
-    if response.status_code == 200:
-        return response.json()
-    else:
-        response.raise_for_status()
+        if index is None:
+            return None
+        
+        api_key = os.getenv(f'VIRUSTOTAL_API_{index}')       
+        headers = {"x-apikey": api_key}
+        
+        response = requests.get(f"https://www.virustotal.com/api/v3/files/{file_hash}", headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            index, api_key = get_virustotalkey(index)

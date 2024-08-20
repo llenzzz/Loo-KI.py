1b23aa62a196c15e02e4a84bc752886a22d0c05b
Michael Gabriel A. Del Rosario <92383206+Subprograms@users.noreply.github.com>
2024-08-20 17:31:21 +0800

    Edited url
    Returns whole data to be dissected in export_csv

--- url.py
+++ url.py
@@ -3,11 +3,4 @@ import whois
 def who_is(url):
     url = url.strip()
     domain = whois.whois(url)
-    
-    domain_info = [
-        domain.domain_name,
-        domain.registrar,
-        domain.name_servers
-    ]
-
-    return domain_info
+    return domain
